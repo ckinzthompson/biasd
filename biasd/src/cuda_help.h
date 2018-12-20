5967bc70b7571223f7f06849a4db18733937fc43
Colin Kinz-Thompson <ckinztho@gmail.com>
2018-12-20 15:48:12 -0500

    update

--- biasd/src/cuda_help.h
+++ biasd/src/cuda_help.h
@@ -9,6 +9,8 @@ extern "C" int cuda_errors(int);
 __device__ double atomicAdd(double* address, double val);
 #endif
 
-__global__ double cuda_parallel_sum(double * a_d, int N, int num_SMs);
+__global__ void cuda_parallel_sum(double *in, int num_elements, double *sum);
+__global__ double parallel_sum(double * a_d, int N, int num_SMs);
 
+int get_padding(int device, int N);
 #endif
