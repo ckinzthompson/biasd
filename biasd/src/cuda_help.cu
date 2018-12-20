d18602d6af45b502a98f819ba044fcca0909bd38
Colin Kinz-Thompson <ckinztho@gmail.com>
2018-12-20 15:54:34 -0500

    swap

--- biasd/src/cuda_help.cu
+++ biasd/src/cuda_help.cu
@@ -1,24 +1,8 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <cuda.h>
 
 #define WARP_SIZE 32
-
-// https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html
-#if __CUDA_ARCH__ < 600
-__device__ double atomicAdd(double* address, double val) {
-	unsigned long long int* address_as_ull =(unsigned long long int*)address;
-	unsigned long long int old = *address_as_ull, assumed;
-
-	do {
-		assumed = old;
-		old = atomicCAS(address_as_ull, assumed,__double_as_longlong(val + __longlong_as_double(assumed)));
-
-	// Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
-	} while (assumed != old);
-
-	return __longlong_as_double(old);
-}
-#endif
 
 int device_count() {
 	int count;
