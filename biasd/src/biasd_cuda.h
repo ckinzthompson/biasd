28aaa8d9c83f468ffffd34b0496b71c6af2c60f8
Colin Kinz-Thompson <ckinztho@gmail.com>
2018-12-20 17:42:04 -0500

    the last jedi

--- biasd/src/biasd_cuda.h
+++ biasd/src/biasd_cuda.h
@@ -8,7 +8,7 @@
 // BIASD related functions
 __global__ void kernel_loglikelihood(int N, double * d, double ep1, double ep2, double sigma1, double sigma2, double k1, double k2, double tau, double epsilon, double * ll);
 
-extern "C" void load_data(int device, int N, double * d, double * ll, void * d_d, void * ll_d);
+extern "C" void load_dataz(int device, int N, double * d, double * ll, void * d_d, void * ll_d);
 extern "C" void free_data(void *d_d, void *ll_d);
 extern "C" void log_likelihood(int device, int N, void *d_d, void *ll_d, double ep1, double ep2, double sigma1, double sigma2, double k1, double k2, double tau, double epsilon, double * ll);
 extern "C" double sum_log_likelihood(int device, int N, void *d_d, void *ll_d, double ep1, double ep2, double sigma1, double sigma2, double k1, double k2, double tau, double epsilon);
