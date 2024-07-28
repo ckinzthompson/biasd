c23c1c1c86f5e847f19dca83d0998b043ba92401
Colin Kinz-Thompson <colin.kinzthompson@rutgers.edu>
2024-07-27 22:30:23 -0400

    cleanup

--- example_data/simulate_trajectories/__init__.py
+++
@@ -1,4 +0,0 @@
-'''
-Use this module to simulate trajectories using the Stochastic Simulation Algorithm (SSA)
-'''
-from sm_sim import ssa_dwells, render_trajectory, simulate, test
--- launch_GUI.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-®
-
-import biasd as b
-b.gui.launch()
