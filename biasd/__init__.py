d1636c702bef3f84e646a3bb8f0bdaeed43ce3ef
Colin Kinz-Thompson <colin.kinzthompson@rutgers.edu>
2024-11-14 15:20:14 -0500

    updates install process

--- biasd/__init__.py
+++ biasd/__init__.py
@@ -2,6 +2,12 @@
 BIASD - Bayesian Inference for the Analysis of Sub-temporal resolution Data
 """
 
+__description__ = "BIASD - Bayesian Inference for the Analysis of Sub-temporal resolution Data."
+__author__ = "Colin Kinz-Thompson"
+__license__ = "MIT"
+__url__ = "https://github.com/ckinzthompson/biasd"
+__version__ = "0.2.3"
+
 from . import likelihood, distributions, mcmc
 from . import laplace, histogram
 from . import plot
--- setup.py
+++ setup.py
@@ -1,20 +1,47 @@
+from setuptools import setup
+import re
+import io
 import os
 import subprocess
-from setuptools import setup
 from setuptools.command.build_py import build_py
 
-class CustomBuildPyCommand(build_py):
-	def run(self):
-		print("Running CustomBuildPyCommand")
-		# Change the working directory and run the make command
-		make_dir = os.path.join(os.getcwd(), 'biasd/likelihood')
-		print(f"Changing directory to: {make_dir}")
-		subprocess.check_call(['make'], cwd=make_dir)
-		# Proceed with the normal build_py
-		build_py.run(self)
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
 
-setup(
-	cmdclass={
-		'build_py': CustomBuildPyCommand,
-	}
-)
+META_PATH = os.path.join("biasd", "__init__.py")
+META_FILE = read(META_PATH)
+
+def find_meta(meta):
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
+        META_FILE, re.MULTILINE
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError(f"Unable to find __{meta}__ string.")
+
+class CustomBuildPyCommand(build_py):
+    def run(self):
+        print("Running CustomBuildPyCommand")
+        # Change the working directory and run the make command
+        make_dir = os.path.join(os.getcwd(), 'biasd/likelihood')
+        print(f"Changing directory to: {make_dir}")
+        subprocess.check_call(['make'], cwd=make_dir)
+        # Proceed with the normal build_py
+        super().run()
+
+if __name__ == "__main__":
+    setup(
+        version=find_meta("version"),
+        description=find_meta("description"),
+        license=find_meta("license"),
+        url=find_meta("url"),
+        author=find_meta("author"),
+        cmdclass={
+            'build_py': CustomBuildPyCommand,
+        },
+    )
