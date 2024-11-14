f04493610d4f741051f228eab6c500808a99492f
Colin Kinz-Thompson <colin.kinzthompson@rutgers.edu>
2024-11-14 15:00:34 -0500

    updates install

--- biasd/__init__.py
+++ biasd/__init__.py
@@ -2,12 +2,6 @@
 BIASD - Bayesian Inference for the Analysis of Sub-temporal resolution Data
 """
 
-__version__ = "0.2.3"
-__description__ = "BIASD - Bayesian Inference for the Analysis of Sub-temporal resolution Data."
-__license__ = "MIT"
-__url__ = "https://github.com/ckinzthompson/biasd"
-__author__ = "Colin Kinz-Thompson"
-
 from . import likelihood, distributions, mcmc
 from . import laplace, histogram
 from . import plot
--- setup.py
+++ setup.py
@@ -1,31 +1,7 @@
-import io
 import os
-import re
 import subprocess
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.command.build_py import build_py
-
-def read(*parts):
-	"""
-	Build an absolute path from *parts* and and return the contents of the
-	resulting file.  Assume UTF-8 encoding.
-	"""
-	HERE = os.path.abspath(os.path.dirname(__file__))
-	with io.open(os.path.join(HERE, *parts), encoding="utf-8") as f:
-		return f.read()
-
-def find_meta(meta):
-	"""
-	Extract __*meta*__ from META_FILE.
-	"""
-	META_FILE = read(os.path.join("biasd","__init__.py"))
-	meta_match = re.search(
-		r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
-		META_FILE, re.M
-	)
-	if meta_match:
-		return meta_match.group(1)
-	raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
 
 class CustomBuildPyCommand(build_py):
 	def run(self):
@@ -38,40 +14,7 @@ class CustomBuildPyCommand(build_py):
 		build_py.run(self)
 
 setup(
-	name='biasd',
-	description=find_meta("description"),
-	license=find_meta("license"),
-	url=find_meta("url"),
-	version=find_meta("version"),
-	author=find_meta("author"),
-	packages=find_packages(),
-	include_package_data=True,
-	package_data={
-		# Include all .so files in the biasd/likelihood directory
-		'biasd.likelihood': ['*.so'],
-	},
-	install_requires=[
-		'numpy>=1.26,<2.0.0',
-		'numba>=0.59',
-		'matplotlib>=3.9',
-		'h5py>=3.9',
-		'scipy>=1.12',
-		'emcee>=3.1',
-		'corner>=2.2',
-		'tqdm>=4.66'
-	],
-	extras_require={
-		'test': [
-			'sphinx',
-			'sphinx_rtd_theme',
-			'pytest',
-			'pytest-cov',
-			'simulate_singlemolecules @ git+https://github.com/ckinzthompson/simulate_singlemolecules@main'
-		],
-		'gui': [
-		],
-	},
 	cmdclass={
 		'build_py': CustomBuildPyCommand,
-	},
+	}
 )
